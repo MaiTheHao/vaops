9163ab55b0929b54250d7c3b99e64b244868804b
MaiTheHao <anhchangnhanma167@gmail.com>
2026-06-15 18:04:46 +0700

    test: remove @SpringBootTest annotation from VaopsApplicationTests

--- backend/src/test/java/c4f/vannang/vaops/VaopsApplicationTests.java
+++ backend/src/test/java/c4f/vannang/vaops/VaopsApplicationTests.java
@@ -1,9 +1,6 @@
 package c4f.vannang.vaops;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class VaopsApplicationTests {
 
 	@Test
