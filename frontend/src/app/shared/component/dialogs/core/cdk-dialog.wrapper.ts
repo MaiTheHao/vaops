9aaccc6b145e102899cead74789eca1c41d7eecb
MaiTheHao <anhchangnhanma167@gmail.com>
2026-07-19 18:08:41 +0700

    feat(dialog): implement CdkDialogWrapper service

---
+++ frontend/src/app/shared/component/dialogs/core/cdk-dialog.wrapper.ts
@@ -0,0 +1,21 @@
+import { inject, Injectable, Type } from '@angular/core';
+import { Dialog } from '@angular/cdk/dialog';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class CdkDialogWrapper {
+  private readonly cdkDialog = inject(Dialog);
+
+  open<R = any>(
+    component: Type<any>,
+    config: { data: any; width?: string; disableClose?: boolean }
+  ): Observable<R | undefined> {
+    const dialogRef = this.cdkDialog.open<R>(component, {
+      width: config.width || '440px',
+      disableClose: config.disableClose ?? true,
+      data: config.data,
+      backdropClass: 'custom-dialog-backdrop'
+    });
+    return dialogRef.closed;
+  }
+}
