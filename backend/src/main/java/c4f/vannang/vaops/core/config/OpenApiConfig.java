3554b174c2713c01ddfe8a1d2d7c6b2bfbcb01e0
MaiTheHao <anhchangnhanma167@gmail.com>
2026-08-09 13:17:53 +0700

    feat: configure OpenAPI security schemes for cookie-based tokens and update API documentation routes

--- backend/src/main/java/c4f/vannang/vaops/core/config/OpenApiConfig.java
+++ backend/src/main/java/c4f/vannang/vaops/core/config/OpenApiConfig.java
@@ -1,24 +1,38 @@
 package c4f.vannang.vaops.core.config;
 
+import c4f.vannang.vaops.core.constant.AuthConstant;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import c4f.vannang.vaops.core.constant.AuthConstant;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-
 @Configuration
 @Profile("!prod")
 @ConditionalOnProperty(value = "vaops.openapi.enabled", havingValue = "true", matchIfMissing = true)
 public class OpenApiConfig {
-  
+
   @Bean
   public OpenAPI customOpenApi() {
     return new OpenAPI()
-      .components(
-        new Components().addSecuritySchemes(AuthConstant.ACCESS_TOKEN_KEY, null)
-      );
+        .components(new Components()
+            .addSecuritySchemes(
+                AuthConstant.ACCESS_TOKEN_KEY,
+                new SecurityScheme()
+                    .type(SecurityScheme.Type.APIKEY)
+                    .in(SecurityScheme.In.COOKIE)
+                    .name(AuthConstant.ACCESS_TOKEN_KEY))
+            .addSecuritySchemes(
+                AuthConstant.REFRESH_TOKEN_KEY,
+                new SecurityScheme()
+                    .type(SecurityScheme.Type.APIKEY)
+                    .in(SecurityScheme.In.COOKIE)
+                    .name(AuthConstant.REFRESH_TOKEN_KEY)))
+        .addSecurityItem(
+            new SecurityRequirement()
+                .addList(AuthConstant.ACCESS_TOKEN_KEY));
   }
 }
